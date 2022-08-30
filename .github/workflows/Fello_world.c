05699499665ee5d57e4ca1bb63541dde6cb022c2
dwham <drkwham2@gmail.com>
2022-08-29 21:42:40 -0600

    Create Fello_world.c

---
+++ .github/workflows/Fello_world.c
@@ -0,0 +1,21 @@
+##this will be a program I have been inspired to write titled fellow-wurld its an attempt to extend hello world print on this screen but simultaniously send out an encrpyted message
+
+var bool checker
+var int x,y,z
+char * message
+
+x=y=z=0
+
+printf("Whats your name");
+x=x+1;
+char * input = scanf("%d", &input);
+  y=y+1;
+  checker == true;
+  message = ('hello from a fello world');
+  
+printf("%d", &message);
+z=z+1;
+
+if (z == x){
+printf("End of Message");
+}
